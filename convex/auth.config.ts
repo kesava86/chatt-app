53e5152a8daaedd8a093b798f3daf1e38e483929
kesava86 <kkesava493@gmail.com>
2024-07-12 11:00:09 +0530

    sample commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig ={
+    providers: [
+      {
+        domain: "https://resolved-shark-70.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [{ hostname: "spotted-koala-528.convex.cloud" },],
+	},
+};
+
+export default nextConfig;
